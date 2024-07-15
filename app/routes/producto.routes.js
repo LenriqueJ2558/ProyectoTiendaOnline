57ba0c19e3093aa854f5e3f93f1cd486a29ada55
Luis Enrique <LenriqueJ2558@gmail.com>
2024-07-15 17:36:27 -0500

    introduci el authJwt en la ruta post Producto

--- app/routes/producto.routes.js
+++ app/routes/producto.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer')
 const { CloudinaryStorage } = require('multer-storage-cloudinary')
 const cloudinary = require('cloudinary').v2
-
+const { authJwt } = require("../middlewares");
 const productController  = require('../controllers/product.controller');
 
 // Configurar Cloudinary
@@ -23,8 +23,8 @@ cloudinary.config({
   })
   const parser = multer({ storage })
 
-router.post('/products',parser.fields([{ name: 'image', maxCount: 1 }]), productController.createProduct);
-router.get('/products',productController.productFilters, productController.getAllProducts);
+router.post('/products', [authJwt.verifyToken, authJwt.isAdmin],parser.fields([{ name: 'image', maxCount: 1 }]), productController.createProduct);
+router.get('/products',productController.productFilters,productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.put('/products/:id', parser.fields([{ name: 'image', maxCount: 1 }]),productController.updateProduct);
 router.delete('/products/:id', productController.deleteProduct);
